6816176c89bd29d7d3cfbe22b717a284f9bfb16a
vanwyk <vanwykch@gmail.com>
2016-12-10 16:53:24 -0700

    Used default base class for main activity.

--- app/src/main/java/com/vwapps/simplescoresheet/MainActivity.java
+++ app/src/main/java/com/vwapps/simplescoresheet/MainActivity.java
@@ -12,13 +12,14 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity
+public class MainActivity extends AppCompatActivity
         implements
         DialogFragmentEnterScore.NoticeDialogListener,
         DialogFragmentAddPlayer.NoticeDialogListener,
